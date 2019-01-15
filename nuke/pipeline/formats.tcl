a98370c26e31e72bee768aceaf6482536feee711
Ned Wilson <ned@qppe.tech>
2019-01-14 16:17:31 -0800

    Initial commit for Romeo start in new Romeo branch

---
+++ nuke/pipeline/formats.tcl
@@ -0,0 +1,27 @@
+add_format "4971 2349	        		 1.0   Alexa 65 VFX Scan Full Ap"
+add_format "4196 2268                    1.0   Alexa 65 VFX Scan Projection + 5% Safety"
+add_format "3996 2160                    1.0   Alexa 65 VFX Scan 1.85 Area"
+add_format "3866 1828                    1.0   Alexa 65 VFX Delivery Full Ap"
+
+add_format "4334 3016	        		 1.0   Alexa LF VFX Scan Full Ap"
+add_format "4196 2268                    1.0   Alexa LF VFX Scan Projection + 5% Safety"
+add_format "3996 2160                    1.0   Alexa LF VFX Scan 1.85 Area"
+add_format "3370 2346                    1.0   Alexa LF VFX Delivery Full Ap"
+
+add_format "4172 1740	        		 1.0   Alexa LF 2.39 VFX Scan Full Ap"
+add_format "3272 1740                    1.0   Alexa LF 2.39 VFX Scan Projection + 5% Safety"
+add_format "3108 1680                    1.0   Alexa LF 2.39 VFX Scan 1.85 Area"
+add_format "4172 1740                    1.0   Alexa LF 2.39 VFX Delivery Full Ap"
+
+add_format "3424 2202	        		 1.0   Alexa Mini VFX Scan Full Ap"
+add_format "3417 1847                    1.0   Alexa Mini VFX Scan Projection + 5% Safety"
+add_format "3254 1759                    1.0   Alexa Mini VFX Scan 1.85 Area"
+add_format "3270 2104                    1.0   Alexa Mini VFX Delivery Full Ap"
+
+add_format "3840 2160	        		 1.0   Blackmagic VFX Scan Full Ap"
+add_format "3840 2160                    1.0   Blackmagic VFX Scan Projection + 5% Safety"
+add_format "3840 2076                    1.0   Blackmagic VFX Scan 1.85 Area"
+add_format "3108 1748                    1.0   Blackmagic VFX Delivery Full Ap"
+
+add_format "3272 1768	        		 1.0   Romeo VFX Delivery Projection + 5% Safety"
+add_format "3108 1680                    1.0   Romeo VFX Delivery Projection 1.85 Area"
--- nuke/pipeline/init.py
+++ nuke/pipeline/init.py
@@ -2,5 +2,8 @@
 
 import nuke
 
+# custom formats
+nuke.load("formats.tcl")
+
 # viewer process register
 # nuke.ViewerProcess.register("EscapeRoom Show LUT", nuke.Node, ("EscapeRoom_ShowLUT", ""))
